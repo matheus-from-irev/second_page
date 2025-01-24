56b993d155476fbb41c04d347a9a8ca8ab120851
matheus-from-irev <matheus.m@irev.com>
2025-01-24 16:32:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.getElementById("form").addEventListener("submit", function(event) {
-    event.preventDefault();
+    
+    var postback = "https://demo-igaming.irev.com/backend/api/v3/goal-type-fire/14?hash=REPLACE"
+    var redirectionPage = "https://matheus-from-irev.github.io/third_page/"
+
     var formData = new FormData(document.getElementById("form"));
     var formDataString = new URLSearchParams(formData).toString();
-
-    let clickId = ""
-    clickId = new URLSearchParams(window.location.search).get("click_id");
-
-    window.location.href = "https://matheus-from-irev.github.io/third_page/?" + formDataString + "&click_id=" + clickId;
+    const clickId = new URLSearchParams(window.location.search).get("click_id");
+    event.preventDefault();
+    postback = postback.replace("REPLACE", clickId);
+    window.location.href = redirectionPage + "?" + formDataString + "&click_id=" + clickId;
   });
