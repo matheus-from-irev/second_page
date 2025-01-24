fa4411830cdb80b60e2b9b32598098d174661289
matheus-from-irev <matheus.m@irev.com>
2025-01-24 14:56:02 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var formData = new FormData(document.getElementById("form"));
+    var formDataString = new URLSearchParams(formData).toString();
+
+    let clickId = ""
+    clickId = new URLSearchParams(window.location.search).get("click_id");
+
+    window.location.href = "https://example.com?" + formDataString + "&click_id=" + clickId;
+  });
